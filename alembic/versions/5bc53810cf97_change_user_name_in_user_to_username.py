4a1b4a500e5bbca7ee30a4dd41280828348f03db
Dmitry Polyakov <polds@list.ru>
2021-04-01 17:32:51 +0300

    requirements fix

---
+++ alembic/versions/5bc53810cf97_change_user_name_in_user_to_username.py
@@ -0,0 +1,24 @@
+"""change user_name in User to username
+
+Revision ID: 5bc53810cf97
+Revises: 4611e9fbd7f0
+Create Date: 2021-04-01 16:44:02.571017
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5bc53810cf97'
+down_revision = '4611e9fbd7f0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
