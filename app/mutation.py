88e37228cdf503bbdfd51c251e01b43f6696ec24
Dmitry Polyakov <polds@list.ru>
2021-04-05 16:15:48 +0300

    add email and nickname repeat check

--- app/mutation.py
+++ app/mutation.py
@@ -16,17 +16,17 @@ class RegisterUser(Mutation):
 
     ok = Boolean()
     id = ID()
+    message = String()
 
     def mutate(root, info, user_data):
-        if db.query(User).filter_by():
-            # ошибка повторения
+        if db.query(User).filter_by(email=user_data.email):
+            return RegisterUser(ok=False, message="An account is already registered for this mailbox")
+        if db.query(User).filter_by(nickname=user_data.nickname):
+            return RegisterUser(ok=False, message="An account is already registered for this nickname")
             pass
-        if db.query(User).filter_by():
-            # ошибка повторения
-            pass
-        db.add(User(email=user_data.email, password_hash=user_data.password, name=user_data.username))
+        db.add(User(email=user_data.email, password_hash=user_data.password, user_name=user_data.username))
         db.commit()
-        return RegisterUser(ok=True, id=db.query(User.id).filter_by(email=user_data.email))
+        return RegisterUser(ok=True, message="Registration done!", id=db.query(User.id).filter_by(email=user_data.email))
 
 
 class Mutation(ObjectType):
