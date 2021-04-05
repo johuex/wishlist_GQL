366bd2dc56c422fc0799d52f825f2d8726a5afb8
Dmitry Polyakov <polds@list.ru>
2021-04-05 17:16:09 +0300

    add auth-token logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import graphene
 from fastapi import FastAPI, APIRouter, Request, Depends
-from fastapi.security import HTTPBasic
+# from fastapi.security import HTTPBasic
 
 from graphql.execution.executors.asyncio import AsyncioExecutor
 from starlette.graphql import GraphQLApp
@@ -18,7 +18,7 @@ models.Base.metadata.create_all(bind=engine)
 def create_app():
     router = APIRouter()
     app = FastAPI()
-    security = HTTPBasic()
+    #security = HTTPBasic()
     gql_app = GraphQLApp(
         schema=graphene.Schema(
             query=Query,
@@ -27,11 +27,13 @@ def create_app():
         graphiql=True
         )
 
+    @router.api_route("/", methods=["GET", "POST"])
     @router.api_route("/gql", methods=["GET", "POST"])
     async def graphql(request: Request):
         return await gql_app.handle_graphql(request=request)
 
-    app.include_router(router, dependencies=[Depends(security)])
+    app.include_router(router)
+    #app.include_router(router, dependencies=[Depends(security)])
 
     return app
 
---
+++ app/auth.py
@@ -0,0 +1,46 @@
+import jwt
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from app.config import Config
+
+
+class AuthHandler:
+    """token authorization"""
+    security = HTTPBearer()
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    secret = Config.SECRET_KEY
+
+    def get_password_hash(self, password):
+        """password -> password_hash"""
+        return self.pwd_context.hash(password)
+
+    def verify_password(self, plain_password, hashed_password):
+        """check password by password_hash"""
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def encode_token(self, user_id):
+        """creating token"""
+        payload = {
+            'exp': datetime.utcnow() + timedelta(days=0, minutes=5),
+            'iat': datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(
+            payload,
+            self.secret,
+            algorithm='HS256')
+
+    def decode_token(self, token):
+        try:
+            payload = jwt.decode(token, self.secret, algorithm=['HS256'])
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=401, detail='Signature has expired')
+        except jwt.InvalidTokenError:
+            raise HTTPException(status_code=401, detail='Invalid token')
+
+    def auth_wrapper(self, auth: HTTPAuthorizationCredentials = Security(security)):
+        """"""
+        return self.decode_token(auth.credentials)
