853802ae191a97e793787264a5a1e05468aec9c2
Dmitry Polyakov <polds@list.ru>
2021-05-31 13:51:43 +0300

    update CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 import graphene
-#from fastapi import FastAPI, APIRouter, Request
 
 from graphql.execution.executors.asyncio import AsyncioExecutor
 from starlette.applications import Starlette
 from starlette.graphql import GraphQLApp
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 from starlette.routing import Route
 
 from app.queries import Query
@@ -21,27 +22,8 @@ def create_app():
                               executor_class=AsyncioExecutor,
                               graphiql=True,))
     ]
-
-    app = Starlette(routes=routes)
+    middleware = [Middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['*'])]
+    app = Starlette(routes=routes, middleware=middleware)
     return app
-'''
-    router = APIRouter()
-    app = FastAPI()
-    gql_app = GraphQLApp(
-        schema=graphene.Schema(
-            query=Query,
-            mutation=Mutation),
-        executor_class=AsyncioExecutor,
-        graphiql=True,
-        )
-
-    @router.api_route("/", methods=["GET", "POST"])
-    @router.api_route("/gql", methods=["GET", "POST"])
-    async def graphql(request: Request):
-        return await gql_app.handle_graphql(request=request)
-
-    app.include_router(router)
-
-    return app'''
 
 
