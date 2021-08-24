8080cbd5f4dab938911a65fa179b74f0b03a6bca
Dmitry <polds@list.ru>
2021-08-24 18:55:45 +0300

    edit $HOME in after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-cd $HOME/wishlist_GQL
+cd /home/ubuntu/wishlist_GQL
 if [ ! -e ./venv ]
 then
   python3 -m virtualenv venv
@@ -10,19 +10,19 @@ source venv/bin/activate
 pip install -r requirements.txt
 if [ ! -e .env ]
 then
-  cp $HOME/deploy/.env $HOME/wishlist_GQL # copy dotenv
+  cp /home/ubuntu/deploy/.env /home/ubuntu/wishlist_GQL # copy dotenv
 fi
 if [ ! -e ./cert ]
 then
-  mkdir $HOME/wishlist_GQL/cert
+  mkdir /home/ubuntu/wishlist_GQL/cert
 fi
 if [ ! -e ./cert/privkey.pem ]
 then
-  cp -H $HOME/deploy/cert/privkey.pem $HOME/wishlist_GQL/cert # copy https key
+  cp -H /home/ubuntu/deploy/cert/privkey.pem /home/ubuntu/wishlist_GQL/cert # copy https key
 fi
 if [ ! -e ./cert/fullchain.pem ]
 then
-  cp -H $HOME/deploy/cert/fullchain.pem $HOME/wishlist_GQL/cert # copy https key
+  cp -H /home/ubuntu/deploy/cert/fullchain.pem /home/ubuntu/wishlist_GQL/cert # copy https key
 fi
 
 # Restart services
